b812f19cf5b3c0aa682538c0bd80da5eef48fd2d
Christopher V <ChrisRVill@gmail.com>
2023-12-08 11:00:00 -0800

    main loop for lines created

--- dayTwo.js
+++ dayTwo.js
@@ -6,21 +6,13 @@ let file = './dayTwoB.txt';
 let rl = readline.createInterface({
     input: fs.createReadStream(file), 
     output: process.stdout,
+    terminal: false
 });
 
 let maxRed = 0;
 let maxBlue = 0;
 let maxGreen = 0;
 
-
-
-const gameMap = {
-    'red': red,
-    'blue': blue,
-    'green': green,
-    'Game': gameID
-}
-gameMap['red'] = 1;
 
 rl.on('line', (text)=> {
     checkGame(text, maxRed, maxBlue, maxGreen);
@@ -29,35 +21,44 @@ rl.on('line', (text)=> {
 
 const checkGame = (line, maxRed, maxBlue, maxGreen)=> {
     let gameMap = {
-        'red': 0,
-        'blue': 0,
-        'green': 0,
+        'red': maxRed,
+        'blue': maxBlue,
+        'green': maxGreen,
         'Game': 0
     }
+    let tempNum = 0;
     let game_entries = Object.entries(gameMap);
+    let lineArray = line.split(' ');
 
-    for(let i = 0; i < line.length; i++){
+    for(let i = 0; i < lineArray.length; i++){
+        let num = parseInt(lineArray[i]);
+        if(num){
+            if(gameMap['Game'] === 0){
+                gameMap['Game'] = num;
+            }
+        }
         for (const [word, value] of game_entries) {
-        const sub = line.substr(i, word.length);
-        if (sub == word) {
-            
+            const sub = lineArray[i].substr(0, word.length);
+            if (sub == word) {
+                console.log(word);
+            }
         }
     }
-    }
-    
     for(const[word, value] of game_entries){
-        if(word)
+        if(word){
+
+        }
     }
     
     return gameMap['Game'];
-}
 
+}   
 
-
-
+/* 
 for (const [digit, word] of digit_entries) {
     const sub = line.substr(i, word.length);
     if (sub == word) {
 
     }
 }
+ */
